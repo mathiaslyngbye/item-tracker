5dec392a4f2d023ca7b81950183f2c0369cd881c
Mathias Lyngbye <mathias@allesen.dk>
2021-07-11 17:36:27 +0200

    Add set and get methods for relevant member variables

--- TrackEntity.hpp
+++ TrackEntity.hpp
@@ -11,7 +11,7 @@ class TrackEntity{
     private:
         void generate_color()
         {
-            cv::RNG rng(12345);
+            cv::RNG rng(cv::getTickCount());
             m_color = cv::Scalar(rng.uniform(0,256), rng.uniform(0,256), rng.uniform(0,256));
         }
         
@@ -24,6 +24,31 @@ class TrackEntity{
         {
             m_bounding_rect = bounding_rect;
             generate_color();
+        }
+
+        cv::Rect getBoundingRect()
+        {
+            return m_bounding_rect;
+        }
+
+        void setBoundingRect(cv::Rect bounding_rect)
+        {
+            m_bounding_rect = bounding_rect;
+        }
+        
+        cv::Scalar getColor()
+        {
+            return m_color;
+        }
+
+        void setId(int id)
+        {
+            m_id = id;
+        }
+        
+        int getId()
+        {
+            return m_id;
         }
 };
 
