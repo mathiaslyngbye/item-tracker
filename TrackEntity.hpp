f491d3ecff398f2bfa44f36d2d6d334f7f371a88
Mathias Lyngbye <mathias@allesen.dk>
2021-08-22 13:38:45 +0200

    Add uuid

--- TrackEntity.hpp
+++ TrackEntity.hpp
@@ -6,16 +6,36 @@
 #include <thread>
 #include <condition_variable>
 #include <queue>
+#include <ctime>
+#include <string>
+#include <sstream>
 
 class TrackEntity{
     private:
-        void generate_color()
+        cv::Scalar generate_color()
         {
             cv::RNG rng(cv::getTickCount());
-            m_color = cv::Scalar(rng.uniform(0,256), rng.uniform(0,256), rng.uniform(0,256));
+            cv::Scalar color = cv::Scalar(rng.uniform(0,256), rng.uniform(0,256), rng.uniform(0,256));
+            return color;
+        }
+        
+        std::string generate_uuid()
+        {
+            std::time_t time_stamp = std::time(0);
+            
+            std::stringstream uuid_ss;
+            uuid_ss << std::to_string(time_stamp);
+            for(int i = 0; i < 3; i++)
+                uuid_ss << '-' << std::setfill('0') << std::setw(3) << (int)m_color[i];
+
+            std::string uuid;
+            uuid_ss >> uuid;
+
+            return uuid;
         }
         
         int m_id = -1;
+        std::string m_uuid;
         cv::Rect m_bounding_rect;
         cv::Scalar m_color; 
     
@@ -23,7 +43,8 @@ class TrackEntity{
         TrackEntity(cv::Rect bounding_rect)
         {
             m_bounding_rect = bounding_rect;
-            generate_color();
+            m_color = generate_color();
+            m_uuid = generate_uuid();
         }
 
         cv::Rect getBoundingRect()
